1fbfe691293b59bc98b42ec055bf54596ca5fee5
Japa Alekhin Llemos <jap@alekhin.io>
2021-04-10 21:47:32 +0800

    added letters helper class

---
+++ lib/helpers/Letters.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+class Letters {
+  static const List<String> letters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  static Random _random = Random();
+
+  static String randomLetter() {
+    return letters[_random.nextInt(letters.length)];
+  }
+}
