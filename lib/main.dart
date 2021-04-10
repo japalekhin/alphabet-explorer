add71d978204cccf917e764a6d1ac26d17db445c
Japa Alekhin Llemos <jap@alekhin.io>
2021-04-10 22:52:10 +0800

    added console runner and ui runner

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:alphabet_explorer/alphabet-explorer.dart';
+import 'package:alphabet_explorer/classes/AlphabetTree.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  runConsole();
+  runWithUi();
+}
+
+void runConsole() {
+  AlphabetTree tree1 = AlphabetTree.random(depth: 3);
+  print(tree1.uniqueLetters);
+
+  AlphabetTree tree2 = AlphabetTree.random(depth: 2);
+  print(tree2.uniqueLetters);
+}
+
+void runWithUi() {
   runApp(AlphabetExplorer());
 }
