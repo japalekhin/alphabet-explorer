16b8ad96c72d81d58edf9bc475c7c174552b1da0
Japa Alekhin Llemos <jap@alekhin.io>
2021-04-10 21:55:21 +0800

    added random named constructor

--- lib/classes/AlphabetTree.dart
+++ lib/classes/AlphabetTree.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:alphabet_explorer/helpers/Letters.dart';
+
 class AlphabetTree {
   final String letter;
   List<AlphabetTree> children = [];
@@ -7,5 +11,15 @@ class AlphabetTree {
     List<AlphabetTree>? children,
   }) {
     this.children = children ?? this.children;
+  }
+
+  AlphabetTree.random({required int depth})
+      : this.letter = Letters.randomLetter() {
+    final Random random = Random();
+
+    final int childrenCount = random.nextInt(5);
+    for (int i = 0; i < childrenCount; i++) {
+      children.add(AlphabetTree.random(depth: depth - 1));
+    }
   }
 }
