8c4f5b1dab10da31be0ca193d84fa8466626e07e
Japa Alekhin Llemos <jap@alekhin.io>
2021-04-11 04:33:42 +0800

    controlled tree for testing purposes

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -5,8 +5,38 @@ import 'package:flutter/material.dart';
 class HomeRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final AlphabetTree tree1 = AlphabetTree.random(
+    final AlphabetTree tree1 = AlphabetTree(
+      letter: 'a',
+      children: [
+        AlphabetTree(
+          letter: 'l',
+          children: [
+            AlphabetTree(
+              letter: 'k',
+            ),
+            AlphabetTree(
+              letter: 'h',
+              children: [
+                AlphabetTree(
+                  letter: 'i',
+                ),
+              ],
+            ),
+          ],
+        ),
+        AlphabetTree(
+          letter: 'e',
+          children: [
+            AlphabetTree(
+              letter: 'n',
+            ),
+          ],
+        ),
+      ],
+    );
+    final AlphabetTree tree2 = AlphabetTree.random(
       depth: 5,
+      minWidth: 1,
       maxWidth: 3,
     );
 
@@ -14,15 +44,32 @@ class HomeRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alphabet Tree Explorer'),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: SingleChildScrollView(
-          child: Center(
-            child: AlphabetTreeWidget(
-              alphabetTree: tree1,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SingleChildScrollView(
+                  child: AlphabetTreeWidget(alphabetTree: tree1),
+                ),
+              ),
             ),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SingleChildScrollView(
+                  child: AlphabetTreeWidget(alphabetTree: tree2),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
