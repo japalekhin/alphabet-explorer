0ed47b198ced384eaefaa2c4d9603201ca815784
Japa Alekhin Llemos <jap@alekhin.io>
2021-04-16 13:33:40 +0800

    a little code formatting

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,15 @@ void main() {
 void runConsole() {
   print('========================================');
   AlphabetTree tree1 = AlphabetTree.random(depth: 3);
-  print('tree1 contains:');
+  print('`tree1` contains:');
   print(tree1.uniqueLetters);
   print('');
 
   AlphabetTree tree2 = AlphabetTree.random(depth: 2);
-  print('tree2 contains:');
+  print('`tree2` contains:');
   print(tree2.uniqueLetters);
   print('');
 
-  print('both trees have:');
   tree1.compare(tree2);
   print('========================================');
 }
